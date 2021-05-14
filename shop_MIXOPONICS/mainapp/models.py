47d06a217730c283f7a6be346a387fb20c1118a0
andrew2004Z <zuikovazd@gmail.com>
2021-05-14 22:31:21 +0300

    Add base.html

--- shop_MIXOPONICS/mainapp/models.py
+++ shop_MIXOPONICS/mainapp/models.py
@@ -7,6 +7,28 @@ from django.contrib.contenttypes.fields import GenericForeignKey
 User = get_user_model()
 
 
+class LatestProductsManager:
+
+    @staticmethod
+    def get_products_for_main_page(*args, **kwargs):
+        with_respect_to = kwargs.get('with_respect_to')
+        products = []
+        ct_models = ContentType.objects.filter(model__in=args)
+        for ct_model in ct_models:
+            model_products = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
+            products.extend(model_products)
+        if with_respect_to:
+            ct_model = ContentType.objects.filter(model=with_respect_to)
+            if ct_model.exists():
+                if with_respect_to in args:
+                    return sorted(products, key=lambda x: x.__class__._meta.model_name.startswith(with_respect_to), reverse=True)
+        return products
+
+
+class LatestProducts:
+
+    objects = LatestProductsManager()
+
 
 class Category(models.Model):
 
@@ -22,12 +44,14 @@ class Product(models.Model):
     class Meta:
         abstract = True
 
-    category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
-    title = models.CharField(max_length=255,verbose_name='Наименование')
+    category = models.ForeignKey(
+        Category, verbose_name='Категория', on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, verbose_name='Наименование')
     slug = models.SlugField(unique=True)
     image = models.ImageField(verbose_name='Изображение')
     description = models.TextField(null=True, verbose_name='Описание')
-    price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Цена')
+    price = models.DecimalField(
+        max_digits=9, decimal_places=2, verbose_name='Цена')
 
     def __str__(self):
         return self.title
@@ -48,21 +72,25 @@ class Seed(Product):
 
     variety = models.CharField(max_length=255, verbose_name='Сорт')
     description = models.TextField(null=True, verbose_name='Описание')
-    col = models.CharField(max_length=255, verbose_name='Количество семян в упаковке')
+    col = models.CharField(
+        max_length=255, verbose_name='Количество семян в упаковке')
 
     def __str__(self):
         return f'{self.category.name}: {self.title}'
-    
+
 
 class CartProduct(models.Model):
 
-    user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
-    cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_products')
+    user = models.ForeignKey(
+        'Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', verbose_name='Корзина',
+                             on_delete=models.CASCADE, related_name='related_products')
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
     qty = models.PositiveIntegerField(default=0)
-    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
+    final_price = models.DecimalField(
+        max_digits=9, decimal_places=2, verbose_name='Общая цена')
 
     def __str__(self):
         return f"Продукт: {self.product.title} (для корзины)"
@@ -70,18 +98,22 @@ class CartProduct(models.Model):
 
 class Cart(models.Model):
 
-    owner = models.ForeignKey('Customer', verbose_name='Владелец', on_delete=models.CASCADE)
-    product = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart')
+    owner = models.ForeignKey(
+        'Customer', verbose_name='Владелец', on_delete=models.CASCADE)
+    product = models.ManyToManyField(
+        CartProduct, blank=True, related_name='related_cart')
     total_products = models.PositiveIntegerField(default=0)
-    final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
+    final_price = models.DecimalField(
+        max_digits=9, decimal_places=2, verbose_name='Общая цена')
 
     def __str__(self):
         return str(self.id)
 
 
 class Customer(models.Model):
-    
-    user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.CASCADE)
+
+    user = models.ForeignKey(
+        User, verbose_name='Пользователь', on_delete=models.CASCADE)
     phone = models.CharField(max_length=20, verbose_name='Номер телефона')
     address = models.CharField(max_length=255, verbose_name='Адрес')
 
--- shop_MIXOPONICS/mainapp/urls.py
+++ shop_MIXOPONICS/mainapp/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from .views import test_views
 
-urlpatterns = []
+urlpatterns = [
+   path('', test_views, name='base'),
+   ]
